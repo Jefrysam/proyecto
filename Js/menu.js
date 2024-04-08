71f0e4a58c761392dfbc68a6da9090f44c5c1649
Jeffry <guerreeocifuentesjefry7777777@gmail.com>
2024-04-08 09:04:31 -0500

    version de entrega

---
+++ Js/menu.js
@@ -0,0 +1,13 @@
+var Precios = []
+//Aqui deberia crear la logica para los precios pero eso despues jsjs
+
+
+document.getElementById("buttonEnviar").addEventListener('click', function() {
+    document.getElementById('formContainer').style.display = 'flex';
+});
+document.getElementById("buttonEnviar").addEventListener('click', function() {
+    document.getElementById("Menu01").style.display = 'none';   
+});
+document.getElementById("buttonEnviar").addEventListener('click', function() {    
+    document.getElementById("botonEnviar").style.display ="none";
+});
