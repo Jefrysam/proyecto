03109c55c503b2f6e204bb1ffd988692557d13e2
Jeffry <guerreeocifuentesjefry7777777@gmail.com>
2024-04-10 10:29:59 -0500

    solo fue un comentario

--- Js/menu.js
+++ Js/menu.js
@@ -1,6 +1,7 @@
 var Precios = []
 //Aqui deberia crear la logica para los precios pero eso despues jsjs
-
+//hay que crear un id para cada uno o buscar otra forma para tomar el precio mostrado
+//y multiplicarlo por la cantidad ingresada en el label
 
 document.getElementById("buttonEnviar").addEventListener('click', function() {
     document.getElementById('formContainer').style.display = 'flex';
